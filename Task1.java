1415a282bc7051dff7688247fff0bc7601acfb38
SIDDHARTH B <bsiddharth100@gmail.com>
2026-06-10 11:11:02 +0530

    Add files via upload

---
+++ Task1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int sum = 0;
+        while(num > 0) {
+            int temp = num % 10;
+            sum += temp;
+            num/=10;
+        }
+        System.out.println(sum);
+    }
+}
